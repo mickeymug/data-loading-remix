254a47241fdc11430e305dc48d8be128ff32cb09
Michael Meyer <michael@mtmeyer.net>
2022-04-05 22:47:13 +1000

    added api fetch in ts example

--- typescript-example/app/routes/currentWord.tsx
+++ typescript-example/app/routes/currentWord.tsx
@@ -1,4 +1,5 @@
-import { LinksFunction } from "@remix-run/node";
+import { LinksFunction, LoaderFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import WordComponent, { links as wordLinks } from "~/components/Word";
 import wordPageStyles from "~/styles/currentWord.css";
 
@@ -6,10 +7,29 @@ export const links: LinksFunction = () => {
   return [...wordLinks(), { rel: "stylesheet", href: wordPageStyles }];
 };
 
+export const loader: LoaderFunction = async () => {
+  const response = await fetch("https://random-words-api.vercel.app/word", {
+    method: "GET",
+  });
+
+  const responseBody = (await response.json()) as WordAPIResponse;
+  return json<WordAPIObject>(responseBody[0]);
+};
+
 export default function WordRoute() {
+  const data = useLoaderData() as WordAPIObject;
+
   return (
     <main className="current-word-wrapper">
-      <WordComponent word="Guava" meaning="A tropical fruit" />
+      <WordComponent word={data.word} meaning={data.definition} />
     </main>
   );
 }
+
+interface WordAPIObject {
+  word: string;
+  definition: string;
+  pronunciation: string;
+}
+
+type WordAPIResponse = WordAPIObject[];
